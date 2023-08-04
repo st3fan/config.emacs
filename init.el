88e937d01bde458cbdb3a764d82c09ad26cb8c55
Stefan Arentz <stefan@arentz.ca>
2023-08-04 16:30:33 -0400

    Updates

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
-(if (< emacs-major-version 26)
-    (error "requires Emacs 26 or later."))
+(if (< emacs-major-version 29)
+    (error "requires Emacs 29 or later."))
 
 (setq gc-cons-threshold (* 128 1024 1024))
 (setq read-process-output-max (* 1024 1024)) ;; For LSP Servers
@@ -52,54 +52,35 @@
 
 (setq custom-file "~/.emacs.custom.el")
 
-;; ===========================================================================
-;; This is a good looking light theme. Only local fix is to give
-;; mode-line-inactive a better color.
+;;
 
-(use-package spacemacs-theme
-  :defer t
-  :init (progn
-         (load-theme 'spacemacs-light t)
-         (face-spec-set
-           'mode-line-inactive
-           '((default
-               :inherit mode-line)
-             (((class color) (min-colors 88) (background light))
-              :weight light
-              :box (:line-width -1 :color "grey75" :style nil)
-              :foreground "grey20" :background "grey90")
-             (((class color) (min-colors 88) (background dark))
-              :background "black" :foreground "gray70" :box nil)))))
+(defun st3fan/hasenv (name)
+  (not (eq (getenv name) nil)))
 
-;; ===========================================================================
-;; Visualize whitespace beyond 120 columns. Should probably disable
-;; this because I generally prefer lines to wrap.
+(defun st3fan/in-tmux ()
+    (st3fan/hasenv "TMUX"))
+
+;;
+
+(use-package leuven-theme
+  :ensure t
+  :init (load-theme 'leuven :no-confirm))
+
+(use-package doom-modeline
+ :ensure t
+ :init (doom-modeline-mode 1))
 
 (use-package whitespace
   :ensure t
-  :diminish whitespace-mode
+  ;;:diminish whitespace-mode
   :config (progn
             (setq whitespace-line-column 120
                   whitespace-style '(face trailing lines-tail))
             (add-hook 'prog-mode-hook 'whitespace-mode)))
 
-;; ===========================================================================
-;; A simpler mode line.
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-
-;; ===========================================================================
-;; Should probably drop this because I can barely see the different colors.
-
 (use-package rainbow-delimiters
   :ensure t
   :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
-
-
-;; ===========================================================================
-;; Auto clean up whitespace. Both trailing and empty lines.
 
 (use-package whitespace-cleanup-mode
   :ensure t
@@ -108,63 +89,9 @@
             (global-whitespace-cleanup-mode)
             (add-hook 'cc-mode-hook 'whitespace-cleanup-mode)))
 
-;; ===========================================================================
-;; Something I miss from IntelliJ.
-
 (use-package expand-region
   :ensure t
   :init (global-set-key (kbd "M-e") 'er/expand-region))
-
-;; ===========================================================================
-;; Remove all bold and underline faces.
-
-(mapc (lambda (face)
-        (set-face-attribute face nil :weight 'light :underline nil))
-      (face-list))
-
-;; ===========================================================================
-;; It's Magit! A Git Porcelain inside Emacs.
-
-(use-package magit
-  :ensure t
-  :bind (("C-x g" . magit-status)))
-
-;; ===========================================================================
-;; I've not found a good use for this yet.
-
-;;(use-package projectile
-;;  :ensure t
-;;  :config (progn
-;;	    (add-to-list 'projectile-globally-ignored-directories "node_modules")
-;;           (add-to-list 'projectile-globally-ignored-directories "env")
-;;	    (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-;;            (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-;;            (projectile-mode +1)))
-
-;; ===========================================================================
-;; Code in C, Code in C, Java is not the answer, Code in C.
-
-(use-package cc-mode
-  :ensure t
-  :config (setq c-default-style "ellemtel"
-                c-basic-offset 3))
-
-;; ===========================================================================
-;; Small modes that make editing some specific file types more pleasant.
-
-(use-package json-mode
-  :ensure t)
-
-(use-package markdown-mode
-  :ensure t)
-
-(use-package yaml-mode
-  :ensure t)
-
-(use-package dockerfile-mode
-  :ensure t)
-
-;; ===========================================================================
 
 (use-package lsp-mode
   :ensure t
@@ -192,8 +119,6 @@
                 lsp-ui-imenu-enable nil
                 lsp-ui-sideline-ignore-duplicate t))
 
-;; ===========================================================================
-
 (use-package company
   :ensure t
   :diminish company-mode
@@ -202,14 +127,16 @@
                 company-minimum-prefix-length 1
                 company-tooltip-align-annotations t))
 
-;; ===========================================================================
-
 (use-package yasnippet
   :ensure t
   :commands yas-minor-mode
   :hook (go-mode . yas-minor-mode))
 
-;; ===========================================================================
+(use-package magit
+  :ensure t
+  :config (setq magit-define-global-key-bindings 'recommended))
+
+;;
 
 (use-package go-mode
   :ensure t
@@ -224,20 +151,27 @@
    (add-hook 'before-save-hook #'lsp-organize-imports t t))
  (add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
-;; ===========================================================================
+(use-package cc-mode
+  :ensure t
+  :config (setq c-default-style "ellemtel"
+                c-basic-offset 3))
 
-(use-package clojure-mode
+(use-package json-mode
   :ensure t)
 
-(use-package cider
+(use-package markdown-mode
   :ensure t)
 
-;; ===========================================================================
+(use-package yaml-mode
+  :ensure t)
+
+(use-package dockerfile-mode
+  :ensure t)
+
+;;
 
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; ===========================================================================
-
-(when (file-exists-p "~/.emacs.local.el")
-  (load "~/.emacs.local.el"))
+(when (file-exists-p "~/.config/emacs/local.el")
+  (load "~/.config/emacs/local.el"))
